35efc593b2f14797280b27a32991cc757247897a
cyij <cyij2006@gmail.com>
2017-07-26 22:56:28 +0800

    add event util

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import $ from './lib/dom'
 import String from './lib/string'
+import $ from './lib/dom'
+import event from './lib/event'
 import util from './lib/util'
 
 export {
   $,
+  event,
   util
 }
